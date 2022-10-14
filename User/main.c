2be4ba68a397002bcf344574923714f69c686c1e
JJ HE <hjj20010422@163.com>
2022-10-14 16:11:00 +0800

    modified:   User/main.c

--- User/main.c
+++ User/main.c
@@ -2,7 +2,7 @@
  * @Author: JJ HE hjj20010422@163.com
  * @Date: 2022-07-26 12:26:58
  * @LastEditors: JJ HE hjj20010422@163.com
- * @LastEditTime: 2022-10-14 16:03:50
+ * @LastEditTime: 2022-10-14 16:10:31
  * @FilePath: \NEWSTD\User\main.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,9 +20,9 @@ int main(void)
 	//uint8_t Myarray[]={'a','b','c'};
 	OLED_Init();
 	Serial_Init();
-	//Serial_SendNum(123456789,9);
-	//Serial_SendString("hhhhhh\r\n");
-	//Serial_SendByte('A');
+	Serial_SendNum(123456789,9);
+	Serial_SendString("hhhhhh\r\n");
+	Serial_SendByte('A');
 //while(a--)
 	//Serial_SendArray(Myarray,3);
  	//Encoder_Init();
