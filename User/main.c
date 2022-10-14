9701caf653acaad55549d24eacbad31f971ea491
JJ HE <hjj20010422@163.com>
2022-10-14 16:04:01 +0800

    modified:   User/main.c

--- User/main.c
+++ User/main.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: JJ HE hjj20010422@163.com
+ * @Date: 2022-07-26 12:26:58
+ * @LastEditors: JJ HE hjj20010422@163.com
+ * @LastEditTime: 2022-10-14 16:03:50
+ * @FilePath: \NEWSTD\User\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"						//加入TIM3定时10ms;给编码器记速
@@ -30,7 +38,7 @@ int main(void)
 	Serial_printf("你好世界");
 	while(1)
 	{
-		
+		OLED_ShowNumber(1,1,55,10);
 //		OLED_ShowSignedNum(1,3,EF,4);
 //		OLED_ShowSignedNum(2,3,ER,4);
 //		OLED_ShowSignedNum(3,3,EL,4);
